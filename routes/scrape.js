7531068e5c53afd8ad5eeeae603c078ba4dcca74
panforrest <guoqianp@gmail.com>
2017-01-29 17:56:53 -0500

    add utils/Scraper.js

--- routes/scrape.js
+++ routes/scrape.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 var cheerio = require('cheerio')
 var superagent = require('superagent')
+var utils = require('../utils')
 
 router.get('/', function(req, res, next){
 	var url =req.query.url
@@ -30,22 +31,23 @@ router.get('/', function(req, res, next){
 
         // res.send(response.text)
         var html = response.text
+        var metaData = utils.Scraper.scrape(html, ['og:title', 'og:description', 'og:image', 'og:url'])
 
-        var props = ['og:title', 'og:description', 'og:image']
-        var metaData = {}
-        $ = cheerio.load(html)
-        $('meta').each(function(i, meta){
-        	if (meta.attribs != null){
-        		var attribs = meta.attribs
-        		if (attribs.property != null){
-        			var prop = attribs.property
-        			if (props.indexOf(prop) != -1){
-        				var key = prop.replace('og:', '')
-        				metaData[key] = attribs.content
-        			}
-        		}
-        	}
-        }) 
+        // var props = ['og:title', 'og:description', 'og:image']
+        // var metaData = {}
+        // $ = cheerio.load(html)
+        // $('meta').each(function(i, meta){
+        // 	if (meta.attribs != null){
+        // 		var attribs = meta.attribs
+        // 		if (attribs.property != null){
+        // 			var prop = attribs.property
+        // 			if (props.indexOf(prop) != -1){
+        // 				var key = prop.replace('og:', '')
+        // 				metaData[key] = attribs.content
+        // 			}
+        // 		}
+        // 	}
+        // }) 
 
         metaData['url'] = url
         res.json({
