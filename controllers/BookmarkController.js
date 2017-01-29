9f57b1a90319d160783a612bd9ac0dcb80ea9703
panforrest <guoqianp@gmail.com>
2017-01-29 18:17:13 -0500

    scraper engine works while submitting an url to bookmark

--- controllers/BookmarkController.js
+++ controllers/BookmarkController.js
@@ -1,5 +1,7 @@
 var Bookmark = require('../models/Bookmark')
 var Promise = require('bluebird')
+var superagent = require('superagent')
+var utils = require('../utils')
 
 module.exports = {
 
@@ -30,15 +32,68 @@ module.exports = {
 	},
 
 	create: function(params){
-    	return new Promise(function(resolve, reject){
-    		Bookmark.create(params, function(err, bookmark){
-    			if (err){
-    				reject(err)
-    				return
-    			}
 
-    			resolve(bookmark)
-    		})
+
+
+
+    	return new Promise(function(resolve, reject){
+
+		    superagent
+			.get(params.url)
+			.query(null)
+			.set('Accept', 'text/html')
+			.end(function(err, response){
+		        if (err){
+                    reject(err)
+
+		        	return
+		        }
+
+		        // res.send(response.text)
+		        var html = response.text
+		        var metaData = utils.Scraper.scrape(html, ['og:title', 'og:description', 'og:image', 'og:url'])
+
+	            Bookmark.create(metaData, function(err, bookmark){
+	    			if (err){
+	    				reject(err)
+	    				return
+	    			}
+
+	    			resolve(bookmark)
+	    		})
+
+		        // var props = ['og:title', 'og:description', 'og:image']
+		        // var metaData = {}
+		        // $ = cheerio.load(html)
+		        // $('meta').each(function(i, meta){
+		        // 	if (meta.attribs != null){
+		        // 		var attribs = meta.attribs
+		        // 		if (attribs.property != null){
+		        // 			var prop = attribs.property
+		        // 			if (props.indexOf(prop) != -1){
+		        // 				var key = prop.replace('og:', '')
+		        // 				metaData[key] = attribs.content
+		        // 			}
+		        // 		}
+		        // 	}
+		        // }) 
+
+		        // metaData['url'] = url
+		        // res.json({
+		        // 	confirmation: 'success',
+		        // 	tags: metaData
+		        // })
+
+			})
+
+    		// Bookmark.create(params, function(err, bookmark){
+    		// 	if (err){
+    		// 		reject(err)
+    		// 		return
+    		// 	}
+
+    		// 	resolve(bookmark)
+    		// })
     	})
     }
 }
