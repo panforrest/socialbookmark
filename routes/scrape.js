05dc1bfcf90698df29aefdf55b25dc11cabce31f
panforrest <guoqianp@gmail.com>
2017-01-29 01:11:29 -0500

    2nd commit of Video-2 http://localhost:3000/scrape returns error message

--- routes/scrape.js
+++ routes/scrape.js
@@ -1,8 +1,18 @@
 var express = require('express')
 var router = express.Router()
+var cheerio = require('cheerio')
+var superagent = require('superagent')
 
 router.get('/', function(req, res, next){
 	var url =req.query.url
+
+	if (url == null){
+		res.json({
+			confirmation: 'fail',
+			message: 'Please enter a valid url'
+		})
+		return
+	}
 
 	res.json({
 		confirmation: 'success',
