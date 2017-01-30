9768262b6a6c901a0c17387a314a101a3023192f
panforrest <guoqianp@gmail.com>
2017-01-29 23:55:06 -0500

    end video4 logout funtion added

--- routes/account.js
+++ routes/account.js
@@ -7,6 +7,13 @@ var utils = require('../utils')
 router.get('/:action', function(req, res, next){
 	var action = req.params.action
 
+	if (action == 'logout'){
+		req.session. reset()
+		res.json({
+			confirmation: 'success'
+		})
+	}
+
 	if (action == 'currentuser'){ //check for a current user
 		if (req.session == null){
 			res.json({
